6a17b790606bed1a709ee6673f87bb0d9fda0200
Philip Muchemi <shemuchemi@gmail.com>
2020-12-20 09:32:50 +0300

    Main acativity layout

--- app/src/main/java/com/okotheffie/remotemonioringapp/Models/User.java
+++ app/src/main/java/com/okotheffie/remotemonioringapp/Models/User.java
@@ -1,39 +1,113 @@
 package com.okotheffie.remotemonioringapp.Models;
 
+import java.util.List;
+
 public class User {
-    private String firstname;
-    private String lastname;
+//    private String firstname;
+//    private String lastname;
+//    private String email;
+//    private String contactnumber;
+//    private String username;
+//    private String user_id;
+//
+//    public User() {
+//    }
+//
+//    public User(String firstname, String lastname, String email, String contactnumber, String username, String user_id) {
+//        this.firstname = firstname;
+//        this.lastname = lastname;
+//        this.email = email;
+//        this.contactnumber = contactnumber;
+//        this.username = username;
+//        this.user_id = user_id;
+//    }
+//    public String getFirstname() {
+//        return firstname;
+//    }
+//    public void setFirstname(String firstname) {
+//        this.firstname = firstname;
+//    }
+//    public String getLastname() {
+//        return lastname;
+//    }
+//    public void setLastname(String lastname) {
+//        this.lastname = lastname;
+//    }
+//    public String getEmail() {
+//        return email;
+//    }
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//    public String getContactnumber() {
+//        return contactnumber;
+//    }
+//    public void setContactnumber(String contactnumber) {
+//        this.contactnumber = contactnumber;
+//    }
+//    public String getUsername() {
+//        return username;
+//    }
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//    public String getUser_id() {
+//        return user_id;
+//    }
+//    public void setUser_id(String user_id) {
+//        this.user_id = user_id;
+//    }
+//    @Override
+//    public String toString() {
+//        return "User{" +
+//                "firstname='" + firstname + '\'' +
+//                ", lastname='" + lastname + '\'' +
+//                ", email='" + email + '\'' +
+//                ", contactnumber='" + contactnumber + '\'' +
+//                ", username='" + username + '\'' +
+//                ", user_id='" + user_id + '\'' +
+//                '}';
+//    }
+
+
+    private String firstName;
+    private String lastName;
     private String email;
-    private String contactnumber;
-    private String username;
-    private String user_id;
+    private String contactNumber;
+    private String userName;
+    private int level;
+    private boolean loggedIn;
+    private String password;
+    private List tasks;
+    private String uid;
 
-    public User() {
-    }
-
-    public User(String firstname, String lastname, String email, String contactnumber, String username, String user_id) {
-        this.firstname = firstname;
-        this.lastname = lastname;
+    public User(String firstName, String lastName, String email, String contactNumber, String userName, int level, boolean loggedIn, String password, List tasks, String uid) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
-        this.contactnumber = contactnumber;
-        this.username = username;
-        this.user_id = user_id;
+        this.contactNumber = contactNumber;
+        this.userName = userName;
+        this.level = level;
+        this.loggedIn = loggedIn;
+        this.password = password;
+        this.tasks = tasks;
+        this.uid = uid;
     }
 
-    public String getFirstname() {
-        return firstname;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public String getLastname() {
-        return lastname;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
@@ -44,39 +118,59 @@ public class User {
         this.email = email;
     }
 
-    public String getContactnumber() {
-        return contactnumber;
+    public String getContactNumber() {
+        return contactNumber;
     }
 
-    public void setContactnumber(String contactnumber) {
-        this.contactnumber = contactnumber;
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
     }
 
-    public String getUsername() {
-        return username;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
-    public String getUser_id() {
-        return user_id;
+    public int getLevel() {
+        return level;
     }
 
-    public void setUser_id(String user_id) {
-        this.user_id = user_id;
+    public void setLevel(int level) {
+        this.level = level;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                ", email='" + email + '\'' +
-                ", contactnumber='" + contactnumber + '\'' +
-                ", username='" + username + '\'' +
-                ", user_id='" + user_id + '\'' +
-                '}';
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List tasks) {
+        this.tasks = tasks;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
     }
 }
